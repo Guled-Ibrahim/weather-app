a33d9eded0b461f19826b118e41ce1d48919a4ae
Guled  Ibrahim <guledibrahim@Guleds-MacBook-Air.local>
2021-11-15 17:34:27 +0000

    weather section template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import { Fragment } from 'react';
-
+import showerIcon from './images/Shower.png';
 const App = () => {
   return (
-    <Fragment className='app__container'>
-      <div className='search__container'></div>
-    </Fragment>
+    <div className='app__container'>
+      <div className='search__container'>
+        <div className='navbar'>
+          <input
+            type='text'
+            placeholder='Enter location'
+            className='navbar__input'
+          />
+          <button className='navbar__btn'>
+            <i className='fas fa-map-marker-alt fa-lg'></i>
+          </button>
+        </div>
+        <div className='weather__container'>
+          <div className='weather__icon'>
+            <img src={showerIcon} alt='shower.png' />
+          </div>
+          <div className='weather__temp'>
+            <span className='temp__one'>1</span>
+            <span className='temp__two'>
+              5<span className='temp__symbol'>&#8451;</span>
+            </span>
+          </div>
+          <div className='weather__info'>
+            <h2 className='weather__text'>shower</h2>
+          </div>
+          <div className='weather__date'>
+            <p className='weather__day'>today</p>
+            <p className='weather__month'>fri, 5 jun</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App';
+import './App.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
