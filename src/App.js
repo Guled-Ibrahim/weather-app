67ffd0f7b1b68365f8d7a7c6d4d73148d5c29bf3
Guled  Ibrahim <guledibrahim@Guleds-MacBook-Air.local>
2021-11-22 20:44:25 +0000

    search weather based on current location

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { useState } from 'react';
 import showerIcon from './images/Shower.png';
 const App = () => {
+  const [city, setCity] = useState('city');
+  const [woeId, setwoeId] = useState(null);
+  const [weatherState, setWeatherState] = useState(null);
+  const [temp, setTemp] = useState(null);
   return (
     <div className='app__container'>
       <div className='search__container'>
@@ -9,8 +14,32 @@ const App = () => {
             placeholder='Enter location'
             className='navbar__input'
           />
-          <button className='navbar__btn'>
-            <i class='fas fa-location-arrow fa-lg'></i>
+          <button
+            className='navbar__btn'
+            onClick={() => {
+              navigator.geolocation.getCurrentPosition((success) => {
+                fetch(
+                  `https://www.metaweather.com/api/location/search/?lattlong=${success.coords.latitude},${success.coords.longitude}`
+                )
+                  .then((res) => res.json())
+                  .then((data) => {
+                    setCity(data[0].title);
+                    setwoeId(data[0].woeid);
+                  });
+                fetch(`https://www.metaweather.com/api/location/${woeId}/`)
+                  .then((res) => res.json())
+                  .then((data) => {
+                    setWeatherState(
+                      data['consolidated_weather'][0]['weather_state_name']
+                    );
+                    setTemp(
+                      data['consolidated_weather'][0]['the_temp'].toFixed(0)
+                    );
+                  });
+              });
+            }}
+          >
+            <i className='fas fa-location-arrow fa-lg'></i>
           </button>
         </div>
         <div className='weather__container'>
@@ -18,21 +47,21 @@ const App = () => {
             <img src={showerIcon} alt='shower.png' />
           </div>
           <div className='weather__temp'>
-            <span className='temp__one'>1</span>
-            <span className='temp__two'>
-              5<span className='temp__symbol'>&#8451;</span>
-            </span>
+            <p className='temp__text'>
+              {temp ? temp : 0}
+              <span className='temp__symbol'>&#8451;</span>
+            </p>
           </div>
           <div className='weather__info'>
-            <h2 className='weather__text'>shower</h2>
+            <h2 className='weather__text'>{weatherState}</h2>
           </div>
           <div className='weather__present'>
             <p className='weather__day'>today</p>
             <p className='weather__date'>fri, 5 jun</p>
           </div>
           <div className='weather__location'>
-            <i class='fas fa-map-marker-alt fa-lg'>
-              <span className='location__text'>London</span>
+            <i className='fas fa-map-marker-alt fa-lg'>
+              <span className='location__text'>{city}</span>
             </i>
           </div>
         </div>
