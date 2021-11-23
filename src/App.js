8e6b7a8e9977b764b649fa6d904f0419968c2a31
Guled  Ibrahim <gman_000@yahoo.com>
2021-11-23 22:01:54 +0000

    weather data loads instantly if location is allowed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import { useState } from 'react';
-import showerIcon from './images/Shower.png';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 const App = () => {
-  const [city, setCity] = useState('city');
-  const [woeId, setwoeId] = useState(null);
+  const [city, setCity] = useState(null);
   const [weatherState, setWeatherState] = useState(null);
   const [temp, setTemp] = useState(null);
-  const [weatherIcon, setWeathericon] = useState(null);
+  const [weatherIcon, setWeatherIcon] = useState();
+
+  useEffect(() => {
+    setTimeout(async () => {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const locationResponse = await axios.get(
+          `https://www.metaweather.com/api/location/search/?lattlong=${position.coords.latitude},${position.coords.longitude}`
+        );
+        const woeIdResponse = await axios.get(
+          `https://www.metaweather.com/api/location/${locationResponse.data[0].woeid}/`
+        );
+        setCity(woeIdResponse.data.title);
+        setTemp(
+          woeIdResponse.data['consolidated_weather'][0][`the_temp`].toFixed(0)
+        );
+        import(
+          `./images/${woeIdResponse.data['consolidated_weather'][0]['weather_state_abbr']}.png`
+        ).then((module) => {
+          setWeatherIcon(module.default);
+        });
+      });
+    }, 1000);
+  });
+
   return (
     <div className='app__container'>
       <div className='search__container'>
@@ -15,57 +37,35 @@ const App = () => {
             placeholder='Enter location'
             className='navbar__input'
           />
-          <button
-            className='navbar__btn'
-            onClick={() => {
-              navigator.geolocation.getCurrentPosition((success) => {
-                fetch(
-                  `https://www.metaweather.com/api/location/search/?lattlong=${success.coords.latitude},${success.coords.longitude}`
-                )
-                  .then((res) => res.json())
-                  .then((data) => {
-                    setCity(data[0].title);
-                    setwoeId(data[0].woeid);
-                  });
-                fetch(`https://www.metaweather.com/api/location/${woeId}/`)
-                  .then((res) => res.json())
-                  .then((data) => {
-                    setWeatherState(
-                      data['consolidated_weather'][0]['weather_state_name']
-                    );
-                    setTemp(
-                      data['consolidated_weather'][0]['the_temp'].toFixed(0)
-                    );
-                  });
-              });
-            }}
-          >
+          <button className='navbar__btn'>
             <i className='fas fa-location-arrow fa-lg'></i>
           </button>
         </div>
-        <div className='weather__container'>
-          <div className='weather__icon'>
-            <img src={showerIcon} alt='shower.png' />
+        {city && (
+          <div className='weather__container'>
+            <div className='weather__icon'>
+              <img src={weatherIcon} alt='icon.png' />
+            </div>
+            <div className='weather__temp'>
+              <p className='temp__text'>
+                {temp ? temp : 0}
+                <span className='temp__symbol'>&#8451;</span>
+              </p>
+            </div>
+            <div className='weather__info'>
+              <h2 className='weather__text'>{weatherState}</h2>
+            </div>
+            <div className='weather__present'>
+              <p className='weather__day'>today</p>
+              <p className='weather__date'>fri, 5 jun</p>
+            </div>
+            <div className='weather__location'>
+              <i className='fas fa-map-marker-alt fa-lg'>
+                <span className='location__text'>{city}</span>
+              </i>
+            </div>
           </div>
-          <div className='weather__temp'>
-            <p className='temp__text'>
-              {temp ? temp : 0}
-              <span className='temp__symbol'>&#8451;</span>
-            </p>
-          </div>
-          <div className='weather__info'>
-            <h2 className='weather__text'>{weatherState}</h2>
-          </div>
-          <div className='weather__present'>
-            <p className='weather__day'>today</p>
-            <p className='weather__date'>fri, 5 jun</p>
-          </div>
-          <div className='weather__location'>
-            <i className='fas fa-map-marker-alt fa-lg'>
-              <span className='location__text'>{city}</span>
-            </i>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
